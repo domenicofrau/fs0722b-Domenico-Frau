387ebe6fda85e1ba52781f8703372fb1ed9f6729
domenicofrau <domenico.frau@outlook.com>
2022-11-10 23:02:53 +0100

    esercizio pom4

--- m1/w3/d3/esercizio_pom_03/js/script.js
+++ m1/w3/d3/esercizio_pom_03/js/script.js
@@ -1,30 +1,38 @@
 
 document.querySelector('#aggiungi').onclick = function () {
     let testo = document.querySelector('#input');
-    if (document.querySelector('#nuovatask input').value.length == 0) {
+    if (document.querySelector('input').value.length == 0) {
         alert("Attenzione! Inserire un promemoria")
     }
     else {
 
-        document.querySelector('#tasks').innerHTML += `
-            <div class="task">
-                <span id="taskname">
-                <i class="fa-regular fa-square check"></i>
-                    ${document.querySelector('#nuovatask input').value}
-                </span>
-                <button class="cancella">
-                    <i class="far fa-trash-alt"></i>
-                </button>
-            </div>
-        `
+        document.querySelector('#tasks').innerHTML += 
+            `
+                <div class="task">
+                    <span class="contenuto">
+                        <i class="fa-regular fa-square check"></i>
+                        ${document.querySelector('input').value}
+                    </span>
+                    <button class="cancella">
+                        <i class="far fa-trash-alt"></i>
+                    </button>
+                </div>
+            `
             ;
-        testo.value = '';
+            testo.value = '';
 
-        let promemoria = document.querySelectorAll(".cancella");
-        for (let i = 0; i < promemoria.length; i++) {
-            promemoria[i].onclick = function () {
-                this.parentNode.remove();
-            }
-        }
+                let cancella = document.querySelectorAll("cancella");
+                for (let i = 0; i < cancella.length; i++) {
+                    cancella[i].onclick = function () {
+                        this.parentNode.remove();
+                    }
+                }
+
+                let sottolinea = document.querySelectorAll("check");
+                for (let i = 0; i < sottolinea.length; i++) {
+                    sottolinea[i].onclick = function () {
+                        this.parentNode.remove();
+                    }
+                }
     }
 }
