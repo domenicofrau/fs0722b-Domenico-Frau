61b1065851209fdaba486813741a77ab6717d64e
domenicofrau <domenico.frau@outlook.com>
2022-11-22 16:55:29 +0100

    sistemazione file git

---
+++ m1/w3/d5/progetto/js/extra.js
@@ -0,0 +1,53 @@
+// [EXTRA] JS Avanzato
+
+/* ESERCIZIO 27
+Crea una funzione chiamata "halfTree" che riceve un numero come parametro e costruisce un mezzo albero di "*" (asterischi) dell'altezza fornita.
+
+Esempio:
+halfTree(3)
+
+*
+**
+***
+
+*/
+
+
+function halfTree (){
+    let x = document.createElement('span').textContent("*");
+}
+
+halfTree ()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* ESERCIZIO 28
+Crea una funzione chiamata "tree" che riceve un numero come parametro e costruisce un albero di "*" (asterischi) dell'altezza fornita.
+
+Esempio:
+tree(3)
+
+  *
+ ***
+*****
+
+*/
+
+/* ESERCIZIO 29
+Crea una funzione chiamata "isItPrime" che riceve un numero come parametro e ritorna true se il numero fornito è un numero primo.
+*/
