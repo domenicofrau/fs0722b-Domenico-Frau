d2d7adc58cf4903367cfdd674c831ec7612b153e
domenicofrau <domenico.frau@outlook.com>
2022-12-19 17:35:50 +0100

    esercizio pom 01

--- m3/w9/d1/e1/giocatore.ts
+++ m3/w9/d1/e1/giocatore.ts
@@ -11,34 +11,38 @@ console.log('Nuomero giocatore 1:', giocatore1);
 console.log('Nuomero giocatore 2 :', giocatore2);
 console.log('----------------------------------')
 
-if (giocatore1 == risultato) {
-    console.log('Il giocatore 1 ha vinto');
-}
-if (giocatore2 == risultato) {
-    console.log('Il giocatore 2 ha vinto');
+
+function confronto() {
+    if (giocatore1 == risultato) {
+        console.log('Il giocatore 1 ha vinto');
+    }
+    if (giocatore2 == risultato) {
+        console.log('Il giocatore 2 ha vinto');
+    }
+
+    if (giocatore1 != risultato && giocatore2 != risultato) {
+        if (giocatore1 < risultato) {
+            comparazione1 = risultato - giocatore1;
+        }
+        else {
+            comparazione1 = giocatore1 - risultato;
+        }
+        if (giocatore2 < risultato) {
+            comparazione2 = risultato - giocatore2;
+        }
+        else {
+            comparazione2 = giocatore2 - risultato;
+        }
+        if (comparazione1 < comparazione2) {
+            console.log('Nessuno dei due giocatori ha indovinato, ma il giocatore 1 si è avvicinato di più');
+        }
+        else {
+            console.log('Nessuno dei due giocatori ha indovinato, ma il giocatore 2 si è avvicinato di più');
+        }
+    }
 }
 
-if (giocatore1 != risultato && giocatore2 != risultato) {
-    if (giocatore1 < risultato) {
-        comparazione1 = risultato - giocatore1; 
-    }
-    else {
-        comparazione1 = giocatore1 - risultato;
-    }
-    if (giocatore2 < risultato) {
-        comparazione2 = risultato - giocatore2; 
-    }
-    else {
-        comparazione2 = giocatore2 - risultato;
-    }
-    if (comparazione1 < comparazione2) {
-        console.log('Nessuno dei due giocatori ha indovinato, ma il giocatore 1 si è avvicinato di più');
-    }
-    else {
-        console.log('Nessuno dei due giocatori ha indovinato, ma il giocatore 2 si è avvicinato di più');
-    }
-}
-
+confronto()
 
 
 
