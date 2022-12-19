13c3e06a2d1cff171f859640506edd505de80870
domenicofrau <domenico.frau@outlook.com>
2022-12-19 16:00:50 +0100

    esercizio pom 01

---
+++ m3/w9/d1/esercizio_pom_01/esercizio 01/giocatore.js
@@ -0,0 +1,37 @@
+var risultato = Math.floor((Math.random() * 100) + 1);
+var giocatore1 = Math.floor((Math.random() * 100) + 1);
+var giocatore2 = Math.floor((Math.random() * 100) + 1);
+var comparazione1;
+var comparazione2;
+console.log('----------------------------------');
+console.log('Nuomero estratto:', risultato);
+console.log('----------------------------------');
+console.log('Nuomero giocatore 1:', giocatore1);
+console.log('Nuomero giocatore 2 :', giocatore2);
+console.log('----------------------------------');
+if (giocatore1 == risultato) {
+    console.log('Il giocatore 1 ha vinto');
+}
+if (giocatore2 == risultato) {
+    console.log('Il giocatore 2 ha vinto');
+}
+if (giocatore1 != risultato && giocatore2 != risultato) {
+    if (giocatore1 < risultato) {
+        comparazione1 = risultato - giocatore1;
+    }
+    else {
+        comparazione1 = giocatore1 - risultato;
+    }
+    if (giocatore2 < risultato) {
+        comparazione2 = risultato - giocatore2;
+    }
+    else {
+        comparazione2 = giocatore2 - risultato;
+    }
+    if (comparazione1 < comparazione2) {
+        console.log('Nessuno dei due giocatori ha indovinato, ma il giocatore 1 si è avvicinato di più');
+    }
+    else {
+        console.log('Nessuno dei due giocatori ha indovinato, ma il giocatore 2 si è avvicinato di più');
+    }
+}
